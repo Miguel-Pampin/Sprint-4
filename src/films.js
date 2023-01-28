f5c2e5ea6da250617ae523e5137f6506d321627d
Miguel <miguelpampiniglesias@gmail.com>
2023-01-28 20:08:46 +0100

    Ejercicio-3

--- src/films.js
+++ src/films.js
@@ -1,27 +1,38 @@
 // Exercise 1: Get the array of all directors.
 
 function getAllDirectors(movies) {
-    let result = movies.map(movie=>movie.director);
+  let result = movies.map(movie => movie.director);
   console.log("EXERCICE 1 ->", result);
-    return result; 
-  
+  return result;
+
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(movies, director) {
-  let result=movies.filter(movie=>movie.director.includes(director));
+  let result = movies.filter(movie => movie.director.includes(director));
   console.log("EXERCICE 2 ->", result);
   return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  
+let moviesFiltered=[];
+let result=0;
+let  director= "Martin Scorsese";
+function moviesAverageOfDirector(movies, director) {
+ moviesFiltered = movies.filter(movie => movie.director.includes(director));
+ moviesAverage();
+ console.log(result);
+ return result;
 }
-
+function moviesAverage(){
+let filmSc=moviesFiltered.map(movie=>movie.score);
+let sumaScore=filmSc.reduce(function(a,b){ return a+b});
+let average=sumaScore/ filmSc.length;
+result=Number(average.toFixed(2));
+}
 // Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
+function orderAlphabetically(movies) {
+
 }
 
 // Exercise 5: Order by year, ascending
@@ -41,7 +52,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
